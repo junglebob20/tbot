9be904eae7c420598efe9ba14bcada69293f7eb5
Artem Veduta <junglebob20@hotmail.com>
2017-10-05 22:04:51 +0200

    Update connectdb.js

--- connectdb.js
+++ connectdb.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  port: 3306,
-  database: "memologdb"
+  host: "ec2-79-125-2-71.eu-west-1.compute.amazonaws.com",
+  user: "yvsvglkuglxfog",
+  password: "600b7696922f50beb0353620185fc400ae5f290c9137ec8dcf9f36e712941491",
+  port: 5432,
+  database: "d4912e1hm4h170"
 });
 con.connect(function(err) {
     if (err) throw err;
